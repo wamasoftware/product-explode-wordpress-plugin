896fc38f747a999995a683bb90cef1bb662a3af2
Samier <samier.sompura@gmail.com>
2018-09-18 11:27:16 +0530

    Final code changes of Product Explode Plugin

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # product-explode-wordpress-plugin
+
+=== Plugin Name ===
+Contributors: (this should be a list of wordpress.org userid's)
+Tags: Product Explode Plugin,prdct_expld,Product Plugin
+Requires at least: 4.6
+Tested up to: 4.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+-- short description here --
+
+Product Explode plugin will give you the functionality to manage single product from group product. User can easly differencite the single product from group of products.
+
+== Description ==
+
+Product Explode plugin will give you the extension or extra panel on product menu on WooCommerce product panel. In that panel, you will be able to see the list of single products under Grouped product Image with labels on wordpress admin side. You can give the size and name of those labels.It also shows the quantity and add to cart option from where you can directly add the product to your cart.
+
+**Features**
+
+* Store administrator can handle the singles products with grouped product.
+* The plugin will displays the number of labels according to those single products in table on WordPress side.
+* Customer can highlight applicable products by clicking on special areas or links on the product image. 
+
+= Product Explode Shortcodes =
+
+
+== Installation ==
+
+**Using FTP**
+
+1. Setup woocommerce plugin.
+2. Upload the entire **Product Explode** folder to the **/wp-content/plugins/** directory.
+3. Activate the plugin through the **Plugins** menu in WordPress admin.
+4. Go to **Products** tab from side menu in Wordpress admin side.
+5. Go to **Product Explode** panel
+
+**Uploading in WordPress Dashboard**
+
+1. Setup woocommerce plugin.
+1. Download `product_explode.zip`.
+1. Navigate to the 'Add New' in the plugins dashboard.
+3. Navigate to the 'Upload' area.
+4. Select `product_explode.zip`from your computer.
+5. Click 'Install Now'.
+6. Activate the Plugin dashboard.
+
+== Screenshots ==
+
+
+== Upgrade Notice ==
+
+= 0.1 =
+
+Starter version no upgrade is required.
+
+== Frequently Asked Questions ==
+
+Que. How to use this plugin?
+Ans. * Activate this plugin by following the installation steps
+     * Go to Product menu on WP side.
+     * Create new or select an existing Grouped Product.
+     * Select Product Explode panel.
+     * It will show you the Product image with table and also the lables with number.
+     * You can arrange the labels according to single product.
+     * You can manage the size of labels and give the text on those labels.
+
+== Changelog ==
+
+= Version 0.1 -july-2018 =
+* New: Store administrator can Specify the single products of a grouped product by using labels
+* New: Store administrator can change the size of labels.
+* New: Store administrator can add small text on labels.
+* New: User can see the same product image on front end with the labels and texts.
--- prod_ex_plgn.php
+++ prod_ex_plgn.php
@@ -7,14 +7,19 @@
  *  Author URI: http://www.wamasoftware.com/
  */
 
-//global $product;
-
 add_action('init', 'ajax_load_scripts');
 
 function ajax_load_scripts()
 {
-    wp_enqueue_script('jQuery', plugin_dir_url(__FILE__) . 'js/script.js', array());
-    wp_enqueue_style('style', plugin_dir_url(__FILE__) . 'css/style.css', array());
+    wp_register_script('jQuery', plugin_dir_url(__FILE__) . 'js/script.js', array());
+    wp_register_style('style', plugin_dir_url(__FILE__) . 'css/style.css', array());
+    wp_register_script('dataTable', plugin_dir_url(__FILE__) . 'js/dataTables.min.js', array());
+    wp_register_style('dataTable', plugin_dir_url(__FILE__) . 'css/dataTables.min.css', array());
+
+    wp_enqueue_script('jQuery');
+    wp_enqueue_style('style');
+    wp_enqueue_script('dataTable');
+    wp_enqueue_style('dataTable');
 }
 
 add_filter('woocommerce_product_data_tabs', 'add_my_custom_product_explode_tab', 99, 1);
@@ -64,7 +69,7 @@ function add_my_custom_product_explode_fields()
                     <span name="label_text_<?php echo $i; ?>" value="<?php echo $label_text; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $label_text; ?></span><?php echo $i; ?>
                     <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $label_left_pos; ?>" class="left position_left_<?php echo $i; ?>">
                 </div>
-        <?php } ?>
+            <?php } ?>
         </div>
         <?php require_once(ABSPATH . 'wp-content/plugins/prod_ex_plgn/associative-product-list.php');
         ?>
@@ -94,18 +99,6 @@ function myplugin_plugin_path()
     return untrailingslashit(plugin_dir_path(__FILE__));
 }
 
-//add_filter('template_include', 'single_prod_template', 99);
-
-//function single_prod_template($template)
-//{
-//    $plugin_path = myplugin_plugin_path() . '/woocommerce/single-product.php';
-//
-//    if (is_page('product')) {
-//        $new_template = locate_template(array($plugin_path));
-//    }
-//    return $plugin_path;
-//}
-
 add_action('woocommerce_after_main_content', 'display_product');
 
 function display_product()
@@ -115,94 +108,100 @@ function display_product()
     $product = get_product(get_the_ID());
     $children = $product->get_children();
     $product_id = $product->get_id();
-    
-    if(!empty($children)){
-      ?>
+
+    if (!empty($children)) {
+        ?>
         <form class="cart grouped_form" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post"enctype="multiple/form-data">
 
-        <!--    Displaying Image    -->
-        <div id="my_custom_product_data" class="panel woocommerce_options_panel">
-            <?php
-            $product_meta = get_post_meta($post->ID);
-            $product_explode_img_url = wp_get_attachment_url($product_meta['_thumbnail_id'][0]);
-            ?> 
-            <img src="<?php echo $product_explode_img_url ?>"> 
-
-            <?php
-            for ($i = 1; $i <= count($children); $i++) {
-
-                $label_width = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
-                $label_height = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
-
-                $label_top_pos = get_post_meta($product_id, "position_top_" . $i, true);
-                $label_left_pos = get_post_meta($product_id, "position_left_" . $i, true);
-
-                $label_text = get_post_meta($product_id, "label_text_" . $i, true);
-                ?>
-                <div data-lableid="<?php echo $i; ?>" id="<?php echo "draggable_" . $i ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $label_left_pos . 'px' ?>; top: <?php echo $label_top_pos . 'px' ?>; width: <?php echo $label_width . 'px' ?>;height: <?php echo $label_height . 'px' ?>;">
-                    <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $label_top_pos; ?>" class="position_top_<?php echo $i; ?>">
-                    <span name="label_text_<?php echo $i; ?>" value="<?php echo $label_text; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $label_text; ?></span><?php echo $i; ?>
-                    <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $label_left_pos; ?>" class="position_left_<?php echo $i; ?>">
-                </div>
+            <!--    Displaying Image    -->
+            <div id="my_custom_product_data" class="panel woocommerce_options_panel">
                 <?php
-            }
-            associative_post_meta($post_ID, $post, $update);
-            $i = 1;
-            ?>
-        </div>
+                $product_meta = get_post_meta($post->ID);
+                $product_explode_img_url = wp_get_attachment_url($product_meta['_thumbnail_id'][0]);
+                ?> 
+                <img src="<?php echo $product_explode_img_url ?>"> 
 
-        <!--    Displaying Products Table-->
-        <div class="product_meta display" id="ass_pr_list_table">
-            <table id="ass_prod_list">
-                <thead>
-                    <tr>
-                        <th>Sr. No.</th>
-                        <th>Product Id</th>
-                        <th>SKU (Item No.)</th>
-                        <th>Product Name</th>
-                        <th>Price</th>
-                        <th>Qty</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
+                <?php
+                for ($i = 1; $i <= count($children); $i++) {
 
-                <tbody>
+                    $label_width = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
+                    $label_height = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
+
+                    $label_top_pos = get_post_meta($product_id, "position_top_" . $i, true);
+                    $label_left_pos = get_post_meta($product_id, "position_left_" . $i, true);
+
+                    $label_text = get_post_meta($product_id, "label_text_" . $i, true);
+                    ?>
+                    <div data-lableid="<?php echo $i; ?>" id="<?php echo "draggable_" . $i ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $label_left_pos . 'px' ?>; top: <?php echo $label_top_pos . 'px' ?>; width: <?php echo $label_width . 'px' ?>;height: <?php echo $label_height . 'px' ?>;">
+                        <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $label_top_pos; ?>" class="position_top_<?php echo $i; ?>">
+                        <span name="label_text_<?php echo $i; ?>" value="<?php echo $label_text; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $label_text; ?></span><?php echo $i; ?>
+                        <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $label_left_pos; ?>" class="position_left_<?php echo $i; ?>">
+                    </div>
                     <?php
-                    foreach ($children as $key => $value) {
-                        $productslist = get_post($value);
-                        $pr = new WC_Product($value);
-                        $post_id = $productslist->ID;
-                        $post_name = $productslist->post_title;
-                        $prod_price = $pr->get_price_html();
-                        $prod_sku = $pr->get_sku();
-                        ?>
-                        <tr class="tr_ass_pr_list" id="tr_ass_pr_list_id_<?php echo $i; ?>" name="tr_ass_pr_list_id_<?php echo $i; ?>">
-                            <td><?php echo $i; ?></td>
-                            <td><?php echo $post_id; ?></td>
-                            <td><?php echo $prod_sku; ?></td>
-                            <td><?php echo $post_name; ?></td>
-                            <td><?php echo $prod_price; ?></td>
-                            <td>
-                                <input type="number" id="quantity_5b3dac65c4092" class="input-text qty text" step="1" min="0" max="100" name="quantity[<?php echo $post_id; ?>]" value="0" title="Qty" size="4" pattern="[0-9]*" inputmode="numeric" aria-labelledby="">
-                            </td>
-                            <td>
-                                <button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
-                            </td>
+                }
+                associative_post_meta($post_ID, $post, $update);
+                $i = 1;
+                ?>
+            </div>
+
+            <!--    Displaying Products Table-->
+            <div class="product_meta display" id="ass_pr_list_table">
+                <table id="ass_prod_list">
+                    <thead>
+                        <tr>
+                            <th>Sr. No.</th>
+                            <th>Product Id</th>
+                            <th>SKU (Item No.)</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Qty</th>
+                            <th>Action</th>
                         </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        foreach ($children as $key => $value) {
+                            $productslist = get_post($value);
+                            $pr = new WC_Product($value);
+                            $post_id = $productslist->ID;
+                            $post_name = $productslist->post_title;
+                            $prod_price = $pr->get_price_html();
+                            $prod_sku = $pr->get_sku();
+                            ?>
+                            <tr class="tr_ass_pr_list" id="tr_ass_pr_list_id_<?php echo $i; ?>" name="tr_ass_pr_list_id_<?php echo $i; ?>">
+                                <td><?php echo $i; ?></td>
+                                <td><?php echo $post_id; ?></td>
+                                <td><?php echo $prod_sku; ?></td>
+                                <td><?php echo $post_name; ?></td>
+                                <td><?php echo $prod_price; ?></td>
+                                <td>
+                                    <input type="number" id="quantity_5b3dac65c4092" class="input-text qty text" step="1" min="0" max="100" name="quantity[<?php echo $post_id; ?>]" value="0" title="Qty" size="4" pattern="[0-9]*" inputmode="numeric" aria-labelledby="">
+                                </td>
+                                <td>
+                                    <button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+                                </td>
+                            </tr>
+                            <?php
+                            $i++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <input type="hidden" name="add-to-cart" value="<?php echo $product_id; ?>"/>
+            </div>
+        </form> 
+
         <?php
-        $i++;
     }
     ?>
-                </tbody>
-            </table>
-            <input type="hidden" name="add-to-cart" value="<?php echo $product_id; ?>"/>
-        </div>
-    </form> 
-    
-    <?php     
-    }
-    ?>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>
+        jQuery('.draggable-label').on('click', function () {
+            var rowid = jQuery(this).data('lableid');
+            jQuery('.tr_ass_pr_list').siblings().removeClass("highlight");
+            jQuery.scrollTo(jQuery('#tr_ass_pr_list_id_' + rowid).addClass("highlight"), 1000);
+        });
+    </script>
     <?php
 }
 
