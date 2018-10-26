797bb834e8959ae1dce01c35725d844493110dc8
Samier <samier.sompura@gmail.com>
2018-10-26 14:12:46 +0530

    Customized code

--- associative-product-list.php
+++ associative-product-list.php
@@ -1,17 +1,14 @@
 <div class="dataTable-area">
-<!--    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">-->
-<!--    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>-->
-
     <?php
 
     // Display Product table on admin
-    function product_explode_table()
+    function productTable()
     {
-        global $wpdb, $product, $i;
+        global $i;
         $i = 1;
-        $product = get_product(get_the_ID());
-        $children = $product->get_children();
-        $product_id = $product->get_id();
+        $productDetails = get_product(get_the_ID());
+        $childProducts = $productDetails->get_children();
+        $product_id = $productDetails->get_id();
         ?>
         <form id="product_form_id" method="post" action="">
             <table border="1" id="product_table_id" class="display">
@@ -26,30 +23,30 @@
 
                 <tbody>
                     <?php
-                    foreach ($children as $key => $value) {
+                    foreach ($childProducts as $key => $value) {
                         $productslist = get_post($value);
-                        $post_id = $productslist->ID;
-                        $post_name = $productslist->post_title;
+                        $productID = $productslist->ID;
+                        $productName = $productslist->post_title;
 
-                        $label_width = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
-                        $label_height = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
+                        $labelWidth = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
+                        $labelHeight = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
 
-                        $label_text = get_post_meta($product_id, "label_text_" . $i, true);
+                        $labelText = get_post_meta($product_id, "label_text_" . $i, true);
                         ?>
 
                         <tr id="<?php echo "tr_product_id_" . $key ?>">
-                            <td class="postId"><?php echo $post_id; ?></td>
-                            <td><?php echo $post_name; ?></td>
+                            <td class="postId"><?php echo $productID; ?></td>
+                            <td><?php echo $productName; ?></td>
                             <td class="label-area-size">
-                                <input type="text" class="label_width" data-lableid="<?php echo $i; ?>" name="<?php echo "pe_label_field_" . $i . "_width"; ?>" id="<?php echo "pe_label_field_" . $i . "_width"; ?>" value="<?php echo $label_width; ?>"> X 
-                                <input type="text" class="label_height" data-lableid="<?php echo $i; ?>" name="<?php echo "pe_label_field_" . $i . "_height"; ?>" id="<?php echo "pe_label_field_" . $i . "_height"; ?>" value="<?php echo $label_height; ?>">
+                                <input type="text" class="label_width" data-lableid="<?php echo $i; ?>" name="<?php echo "pe_label_field_" . $i . "_width"; ?>" id="<?php echo "pe_label_field_" . $i . "_width"; ?>" value="<?php echo $labelWidth; ?>"> X 
+                                <input type="text" class="label_height" data-lableid="<?php echo $i; ?>" name="<?php echo "pe_label_field_" . $i . "_height"; ?>" id="<?php echo "pe_label_field_" . $i . "_height"; ?>" value="<?php echo $labelHeight; ?>">
                             </td>
                             <td class="label-num">
-                                <input type="text" class="label-field-link-to-num" name="<?php echo "label_text_" . $i ?>" id="label_field_link_to_num" value="<?php echo $label_text; ?>">
+                                <input type="text" class="label-field-link-to-num" name="<?php echo "label_text_" . $i ?>" id="label_field_link_to_num" value="<?php echo $labelText; ?>">
                             </td>
                         </tr>
                         <?php
-                        update_post_meta($product_id, "linked_label_$post_id", $i);
+                        update_post_meta($product_id, "linked_label_$productID", $i);
                         $i++;
                     }
                     ?>
@@ -59,5 +56,5 @@
         <?php
     }
     ?>
-    <?php product_explode_table(); ?>
+    <?php productTable(); ?>
 </div>
--- prod_ex_plgn.php
+++ prod_ex_plgn.php
@@ -1,16 +1,15 @@
 <?php
 /*
  *  Plugin Name: Product Explode Plugin
- *  Descrption: This plugin allows you to explode the image of your Group product.
+ *  Description: This plugin allows you to explode the image of your Group product.
  *  Version: 1.0
  *  Author: Wama Software
  *  Author URI: http://www.wamasoftware.com/
  */
 
-add_action('init', 'ajax_load_scripts');
+add_action('init', 'loadScripts');
 
-function ajax_load_scripts()
-{
+function loadScripts() {
     wp_register_script('jQuery', plugin_dir_url(__FILE__) . 'js/script.js', array());
     wp_register_style('style', plugin_dir_url(__FILE__) . 'css/style.css', array());
     wp_register_script('dataTable', plugin_dir_url(__FILE__) . 'js/dataTables.min.js', array());
@@ -25,121 +24,115 @@ function ajax_load_scripts()
 add_filter('woocommerce_product_data_tabs', 'add_my_custom_product_explode_tab', 99, 1);
 
 // Adding Product Explode tab on products menu in Admin
-function add_my_custom_product_explode_tab($product_data_tabs)
-{
-    $product_data_tabs['product-explode'] = array(
+function add_my_custom_product_explode_tab($productTabs) {
+    $productTabs['product-explode'] = array(
         'label' => __('Product Explode', 'my_text_domain'),
         'target' => 'my_custom_product_data',
         'class' => array('show_if_grouped'),
     );
 
-    return $product_data_tabs;
+    return $productTabs;
 }
 
 add_action('woocommerce_product_data_panels', 'add_my_custom_product_explode_fields');
 
-function add_my_custom_product_explode_fields()
-{
-    global $woocommerce, $post;
-    $product = get_product(get_the_ID());
-    $children = $product->get_children();
-    $product_id = $product->get_id();
+function add_my_custom_product_explode_fields() {
+    global $post;
+    $productDetail = get_product(get_the_ID());
+    $childProducts = $productDetail->get_children();
+    $productID = $productDetail->get_id();
     ?>
 
     <!--Displaying product image on product explode tab-->
     <div id="my_custom_product_data" class="panel woocommerce_options_panel">
         <?php
-        $product_meta = get_post_meta($post->ID);
-        $product_explode_img_url = wp_get_attachment_url($product_meta['_thumbnail_id'][0]);
+        $productMetaData = get_post_meta($post->ID);
+        $imgUrl = wp_get_attachment_url($productMetaData['_thumbnail_id'][0]);
         ?>
         <div id="test" class="exploded-area">
-            <img src="<?php echo $product_explode_img_url ?>">
+            <img src="<?php echo $imgUrl ?>">
 
             <?php
-            for ($i = 1; $i <= count($children); $i++) {
-                $label_width = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
-                $label_height = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
+            for ($i = 1; $i <= count($childProducts); $i++) {
+                $labelWidth = get_post_meta($productID, "pe_label_field_" . $i . "_width", true);
+                $labelHeight = get_post_meta($productID, "pe_label_field_" . $i . "_height", true);
 
-                $label_top_pos = get_post_meta($product_id, "position_top_" . $i, true);
-                $label_left_pos = get_post_meta($product_id, "position_left_" . $i, true);
-                $label_text = get_post_meta($product_id, "label_text_" . $i, true);
+                $labelTop = get_post_meta($productID, "position_top_" . $i, true);
+                $labelLeft = get_post_meta($productID, "position_left_" . $i, true);
+                $labelText = get_post_meta($productID, "label_text_" . $i, true);
                 ?>
-                <div id="<?php echo "draggable_" . $i ?>" data-lableid="<?php echo $i; ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $label_left_pos . 'px' ?>; top: <?php echo $label_top_pos . 'px' ?>; width: <?php echo $label_width . 'px' ?>;height: <?php echo $label_height . 'px' ?>;">
-                    <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $label_top_pos; ?>" class="top position_top_<?php echo $i; ?>">
-                    <span name="label_text_<?php echo $i; ?>" value="<?php echo $label_text; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $label_text; ?></span><?php echo $i; ?>
-                    <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $label_left_pos; ?>" class="left position_left_<?php echo $i; ?>">
+                <div id="<?php echo "draggable_" . $i ?>" data-lableid="<?php echo $i; ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $labelLeft . 'px' ?>; top: <?php echo $labelTop . 'px' ?>; width: <?php echo $labelWidth . 'px' ?>;height: <?php echo $labelHeight . 'px' ?>;">
+                    <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $labelTop; ?>" class="top position_top_<?php echo $i; ?>">
+                    <span name="label_text_<?php echo $i; ?>" value="<?php echo $labelText; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $labelText; ?></span><?php echo $i; ?>
+                    <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $labelLeft; ?>" class="left position_left_<?php echo $i; ?>">
                 </div>
             <?php } ?>
         </div>
-        <?php require_once(ABSPATH . 'wp-content/plugins/prod_ex_plgn/associative-product-list.php');
-        ?>
+        <?php require_once('associative-product-list.php'); ?>
     </div>
     <?php
 }
 
 add_action('save_post', 'associative_post_meta', 10, 3);
 
-function associative_post_meta($post_ID, $post, $update)
-{
+function productExplodePostMeta($productID) {
     for ($i = 1; $i <= count($_REQUEST['grouped_products']); $i++) {
-        update_post_meta($post_ID, "pe_label_field_" . $i . "_width", $_POST['pe_label_field_' . $i . '_width']);
-        update_post_meta($post_ID, "pe_label_field_" . $i . "_height", $_POST['pe_label_field_' . $i . '_height']);
+        update_post_meta($productID, "pe_label_field_" . $i . "_width", $_POST['pe_label_field_' . $i . '_width']);
+        update_post_meta($productID, "pe_label_field_" . $i . "_height", $_POST['pe_label_field_' . $i . '_height']);
 
-        update_post_meta($post_ID, "position_top_" . $i, $_POST['position_top_' . $i]);
-        update_post_meta($post_ID, "position_left_" . $i, $_POST['position_left_' . $i]);
+        update_post_meta($productID, "position_top_" . $i, $_POST['position_top_' . $i]);
+        update_post_meta($productID, "position_left_" . $i, $_POST['position_left_' . $i]);
 
-        update_post_meta($post_ID, "label_text_" . $i, $_POST['label_text_' . $i]);
+        update_post_meta($productID, "label_text_" . $i, $_POST['label_text_' . $i]);
     }
 }
 
-function myplugin_plugin_path()
-{
+function myplugin_plugin_path() {
     // gets the absolute path to this plugin directory
 
     return untrailingslashit(plugin_dir_path(__FILE__));
 }
 
-add_action('woocommerce_after_main_content', 'display_product');
+add_action('woocommerce_after_main_content', 'displayProductList');
 
-function display_product()
-{
-    global $wpdb, $product, $woocommerce, $post, $i;
+function displayProductList() {
+    global $wpdb, $product, $post, $i;
 
-    $product = get_product(get_the_ID());
-    $children = $product->get_children();
-    $product_id = $product->get_id();
+    $productDetails = get_product(get_the_ID());
+    $childProducts = $productDetails->get_children();
+    $product_id = $productDetails->get_id();
 
-    if (!empty($children)) {
+    if (!empty($childProducts)) {
         ?>
         <form class="cart grouped_form" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post"enctype="multiple/form-data">
 
             <!--    Displaying Image    -->
             <div id="my_custom_product_data" class="panel woocommerce_options_panel">
                 <?php
-                $product_meta = get_post_meta($post->ID);
-                $product_explode_img_url = wp_get_attachment_url($product_meta['_thumbnail_id'][0]);
+                $productMeta = get_post_meta($post->ID);
+                $explodeImgURL = wp_get_attachment_url($productMeta['_thumbnail_id'][0]);
                 ?> 
-                <img src="<?php echo $product_explode_img_url ?>"> 
+                <img src="<?php echo $explodeImgURL ?>"> 
 
                 <?php
-                for ($i = 1; $i <= count($children); $i++) {
+                for ($i = 1; $i <= count($childProducts); $i++) {
 
-                    $label_width = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
-                    $label_height = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
+                    $labelWidth = get_post_meta($product_id, "pe_label_field_" . $i . "_width", true);
+                    $labelHeight = get_post_meta($product_id, "pe_label_field_" . $i . "_height", true);
 
-                    $label_top_pos = get_post_meta($product_id, "position_top_" . $i, true);
-                    $label_left_pos = get_post_meta($product_id, "position_left_" . $i, true);
+                    $labelTop = get_post_meta($product_id, "position_top_" . $i, true);
+                    $labelLeft = get_post_meta($product_id, "position_left_" . $i, true);
 
-                    $label_text = get_post_meta($product_id, "label_text_" . $i, true);
+                    $labelText = get_post_meta($product_id, "label_text_" . $i, true);
                     ?>
-                    <div data-lableid="<?php echo $i; ?>" id="<?php echo "draggable_" . $i ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $label_left_pos . 'px' ?>; top: <?php echo $label_top_pos . 'px' ?>; width: <?php echo $label_width . 'px' ?>;height: <?php echo $label_height . 'px' ?>;">
-                        <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $label_top_pos; ?>" class="position_top_<?php echo $i; ?>">
-                        <span name="label_text_<?php echo $i; ?>" value="<?php echo $label_text; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $label_text; ?></span><?php echo $i; ?>
-                        <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $label_left_pos; ?>" class="position_left_<?php echo $i; ?>">
+                    <div data-lableid="<?php echo $i; ?>" id="<?php echo "draggable_" . $i ?>" class="ui-widget-content draggable draggable-label" style="left: <?php echo $labelLeft . 'px' ?>; top: <?php echo $labelTop . 'px' ?>; width: <?php echo $labelWidth . 'px' ?>;height: <?php echo $labelHeight . 'px' ?>;">
+                        <input type="hidden" name="position_top_<?php echo $i; ?>" value="<?php echo $labelTop; ?>" class="position_top_<?php echo $i; ?>">
+                        <span name="label_text_<?php echo $i; ?>" value="<?php echo $labelText; ?>" class="label-text label_text_<?php echo $i; ?>"><?php echo $labelText; ?></span><?php echo $i; ?>
+                        <input type="hidden" name="position_left_<?php echo $i; ?>" value="<?php echo $labelLeft; ?>" class="position_left_<?php echo $i; ?>">
                     </div>
                     <?php
                 }
-                associative_post_meta($post_ID, $post, $update);
+                productExplodePostMeta($productID);
                 $i = 1;
                 ?>
             </div>
@@ -161,22 +154,22 @@ function display_product()
 
                     <tbody>
                         <?php
-                        foreach ($children as $key => $value) {
+                        foreach ($childProducts as $key => $value) {
                             $productslist = get_post($value);
-                            $pr = new WC_Product($value);
-                            $post_id = $productslist->ID;
-                            $post_name = $productslist->post_title;
-                            $prod_price = $pr->get_price_html();
-                            $prod_sku = $pr->get_sku();
+                            $getProduct = new WC_Product($value);
+                            $postId = $productslist->ID;
+                            $postName = $productslist->post_title;
+                            $prodPrice = $getProduct->get_price_html();
+                            $prodSku = $getProduct->get_sku();
                             ?>
                             <tr class="tr_ass_pr_list" id="tr_ass_pr_list_id_<?php echo $i; ?>" name="tr_ass_pr_list_id_<?php echo $i; ?>">
                                 <td><?php echo $i; ?></td>
-                                <td><?php echo $post_id; ?></td>
-                                <td><?php echo $prod_sku; ?></td>
-                                <td><?php echo $post_name; ?></td>
-                                <td><?php echo $prod_price; ?></td>
+                                <td><?php echo $postId; ?></td>
+                                <td><?php echo $prodSku; ?></td>
+                                <td><?php echo $postName; ?></td>
+                                <td><?php echo $prodPrice; ?></td>
                                 <td>
-                                    <input type="number" id="quantity_5b3dac65c4092" class="input-text qty text" step="1" min="0" max="100" name="quantity[<?php echo $post_id; ?>]" value="0" title="Qty" size="4" pattern="[0-9]*" inputmode="numeric" aria-labelledby="">
+                                    <input type="number" id="quantity_5b3dac65c4092" class="input-text qty text" step="1" min="0" max="100" name="quantity[<?php echo $postId; ?>]" value="0" title="Qty" size="4" pattern="[0-9]*" inputmode="numeric" aria-labelledby="">
                                 </td>
                                 <td>
                                     <button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
@@ -238,4 +231,3 @@ function myplugin_woocommerce_locate_template($template, $template_name, $templa
     // Return what we found
     return $template;
 }
-?>
